ac0542c43e7955d99fea428bc5a914fe4dc287f4
MrTheGood <maarten@degoedeweb.nl>
2019-03-01 17:30:04 +0100

    Implement cloud firestore

--- src/app/index.js
+++ src/app/index.js
@@ -3,9 +3,15 @@ const views = require('koa-views')
 const path = require('path')
 const router = require('../router')
 const nunjucks = require('nunjucks')
+const admin = require('firebase-admin')
+const serviceAccount = require('../serviceAccountKey')
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://insertcode-portfolio-app.firebaseio.com"
+})
+const firestore = admin.firestore()
 
 const app = new Koa()
-
 const v = path.resolve(__dirname, '../views')
 nunjucks.configure(v)
 app.use(views(v, {
