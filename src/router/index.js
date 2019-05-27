5ad2427e0578060ee47c51614bd5e9e9b5991cf5
MrTheGood <maarten@degoedeweb.nl>
2019-05-27 16:17:18 +0200

    Add about

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,10 @@ const router = require('koa-router')()
 
 const home = require('pages/home')
 const project = require('pages/projects')
+const about = require('pages/about')
 
 router.use('/', home)
 router.use('/projects', project)
+router.use('/about', about)
 
 module.exports = router
